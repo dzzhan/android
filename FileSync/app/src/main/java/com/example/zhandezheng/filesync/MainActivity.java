9f97e4aef3992bd97add774a094bac4e5277057b
dzzhan <dzzhan@163.com>
2018-04-30 21:11:04 +0800

    访问SD卡成功！

--- FileSync/app/src/main/java/com/example/zhandezheng/filesync/MainActivity.java
+++ FileSync/app/src/main/java/com/example/zhandezheng/filesync/MainActivity.java
@@ -2,19 +2,39 @@ package com.example.zhandezheng.filesync;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.io.*;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.ContentUris;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewDebug;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
+import android.widget.TextView;
+import android.Manifest;
+import static android.support.v4.app.ActivityCompat.*;
 
 public class MainActivity extends Activity {
 
     Button btn1;
     Button btn2;
+    TextView filePath;
+    ListView fileList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,26 +46,218 @@ public class MainActivity extends Activity {
 
         btn2 = (Button) findViewById(R.id.btn2);
         btn2.setOnClickListener(new btListener());
+
+        filePath = (TextView) findViewById(R.id.file_path);
+
+        fileList = (ListView) findViewById(R.id.file_list);
     }
 
     private class btListener implements View.OnClickListener{  //自定义监听类，继承OnClickListener
-        public void onClick(View view){                        //实现方法
+        public void onClick(View view){
+            //实现方法
             // TODO Auto-generated method stub
+            Intent intent;
             switch (view.getId()) {
-                case R.id.btn2:
-                    Toast.makeText(MainActivity.this, "This is Button 111", Toast.LENGTH_SHORT).show();
+                case R.id.btn1:
+                    //Toast.makeText(MainActivity.this, "This is Button 111", Toast.LENGTH_SHORT).show();
+                    intent = new Intent(Intent.ACTION_GET_CONTENT);
+                    //intent.setType(“image/*”);//选择图片
+                    //intent.setType(“audio/*”); //选择音频
+                    //intent.setType(“video/*”); //选择视频 （mp4 3gp 是android支持的视频格式）
+                    //intent.setType(“video/*;image/*”);//同时选择视频和图片
+                    intent.setType("*/*");//无类型限制
+                    intent.addCategory(Intent.CATEGORY_OPENABLE);
+                    startActivityForResult(intent, 1);
                     break;
 
-                case R.id.btn1:
+                case R.id.btn2:
                     // TODO Auto-generated method stub
                     //创建需要对应目标Activity的intent
-                    Intent intent=new Intent(MainActivity.this,MainActivity2.class);
+                    //intent=new Intent(MainActivity.this,MainActivity2.class);
                     //启动指定Activity并等待返回的结果，0是请求码。用于表示该请求
-                    startActivity(intent);
+                    //startActivity(intent);
+                    ListFiles();
                     break;
                 default:
                     break;
             }
         }
+    }
+
+    String path;
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            Uri uri = data.getData();
+            if ("file".equalsIgnoreCase(uri.getScheme())){//使用第三方应用打开
+                path = uri.getPath();
+                filePath.setText(path);
+                Toast.makeText(this,path+"11111",Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT) {//4.4以后
+                path = getPath(this, uri);
+                filePath.setText(path);
+                Toast.makeText(this,path,Toast.LENGTH_SHORT).show();
+            } else {//4.4以下下系统调用方法
+                path = getRealPathFromURI(uri);
+                filePath.setText(path);
+                Toast.makeText(MainActivity.this, path+"222222", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private File[] getFiles(String strPath)
+    {
+        Log.i("getFiles from:", strPath);
+        File stFile = new File(strPath);
+        File[] files = stFile.listFiles();
+        return files;
+    }
+
+    private void ListFiles()
+    {
+        //检测读写权限
+        PermisionUtils.verifyStoragePermissions(this);
+        String strSDCardPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).getAbsolutePath().toString() +"/Camera";
+        Log.i("SDCardPath",strSDCardPath);
+        File[] fileList = getFiles(strSDCardPath);
+        if (null == fileList)
+        {
+            Log.i("Warning", "Get file failed!");
+            return;
+        }
+        for (int i = 0; i < fileList.length; i++)
+        {
+            Log.i("Info", fileList[i].getAbsolutePath());
+        }
+    }
+
+    public String getRealPathFromURI(Uri contentUri) {
+        String res = null;
+        String[] proj = { MediaStore.Images.Media.DATA };
+        Cursor cursor = getContentResolver().query(contentUri, proj, null, null, null);
+        if(null!=cursor&&cursor.moveToFirst()){;
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            res = cursor.getString(column_index);
+            cursor.close();
+        }
+        return res;
+    }
+
+    /**
+     * 专为Android4.4设计的从Uri获取文件绝对路径，以前的方法已不好使
+     */
+    @SuppressLint("NewApi")
+    public String getPath(final Context context, final Uri uri) {
+
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+            }
+            // DownloadsProvider
+            else if (isDownloadsDocument(uri)) {
+
+                final String id = DocumentsContract.getDocumentId(uri);
+                final Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
+                return getDataColumn(context, contentUri, null, null);
+            }
+            // MediaProvider
+            else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[]{split[1]};
+
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        }
+        // MediaStore (and general)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            return getDataColumn(context, uri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+        return null;
+    }
+
+    /**
+     * Get the value of the data column for this Uri. This is useful for
+     * MediaStore Uris, and other file-based ContentProviders.
+     *
+     * @param context       The context.
+     * @param uri           The Uri to query.
+     * @param selection     (Optional) Filter used in the query.
+     * @param selectionArgs (Optional) Selection arguments used in the query.
+     * @return The value of the _data column, which is typically a file path.
+     */
+    public String getDataColumn(Context context, Uri uri, String selection,
+                                String[] selectionArgs) {
+
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {column};
+
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+                    null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int column_index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(column_index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is ExternalStorageProvider.
+     */
+    public boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    public boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    public boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
     }
 }
